2ecf5c36938c542658671d28a3b2caf764078382
OleanjiKingCode <holamilean56@gmail.com>
2022-10-30 22:21:02 +0100

    re-evaluating code

--- src/Exc-Contract.sol
+++ src/Exc-Contract.sol
@@ -35,8 +35,14 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
     uint spinBoardPrice = 180;
 
     uint boardItems = 0;
-    address[] public PeopleWhoSpinned;
+    address[] public PeopleWhospinned;
 
+    //address of the deployer
+    address ownerAddress;
+    uint public lastTimeStamp;
+
+
+    ////Chainlink vrf Vars
     address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
 
     bytes32 keyHash =
@@ -48,19 +54,14 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
 
     uint32 numWords = 1;
 
-    uint256 public s_randomLuck;
+    uint256 public randomLuck;
     address public winner;
-    uint256 public s_requestId;
+    uint256 public requestId;
     address s_owner;
-    //address of the deployer
-    address ownerAddress;
-    uint public lastTimeStamp;
-
-    uint mintToNewPlayers = 130;
 
     VRFCoordinatorV2Interface COORDINATOR;
 
-    uint64 s_subscriptionId;
+    uint64 subscriptionId;
 
 
 
@@ -69,9 +70,9 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
     /// -----------------------------------------------------------------------
 
     mapping(address => bool) private areyouAPlayer;
-    mapping(address => bool) private Spinned;
-    mapping(uint => Players) private IdOfPlayers;
-    mapping(address => uint) private AddressOfPlayers;
+    mapping(address => bool) private spinned;
+    mapping(uint => Players) private idOfPlayers;
+    mapping(address => uint) private addressOfPlayers;
        
 
 
@@ -97,15 +98,15 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
     /// -----------------------------------------------------------------------
     constructor(
         uint _totalSupply,
-        uint64 subscriptionId,
+        uint64 subscriptionIdOfVrf,
         uint items_on_board
     ) VRFConsumerBaseV2(vrfCoordinator) ERC20("EXCGameToken", EGT") {
         ownerAddress = msg.sender;
-        no_of_items_on_board = items_on_board;
+        boardItems = items_on_board;
         uint amount = _totalSupply * 10**18;
         _mint(ownerAddress, amount);
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-        s_subscriptionId = subscriptionId;
+        subscriptionIdOfVrf = subscriptionId;
         s_owner = msg.sender;
         lastTimeStamp = block.timestamp;
     }
@@ -123,22 +124,22 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
         require(areyouAPlayer[msg.sender] == false, "you are already a player");
 
         NumOfAllPlayers.increment();
-        uint value_given_to_new_players = mintToNewPlayers * 10**18;
+        uint value_given_to_new_players = gameEntryReward * 10**18;
         _mint(msg.sender, value_given_to_new_players);
         uint[] memory _scores = new uint[](0);
         uint currentplayerId = NumOfAllPlayers.current();
-        IdOfPlayers[currentplayerId] = Players(
+        idOfPlayers[currentplayerId] = Players(
             _name,
             msg.sender,
             currentplayerId,
             0,
             _date,
-            mintToNewPlayers,
+            gameEntryReward,
             _scores,
             0,
             false
         );
-        AddressOfPlayers[msg.sender] = currentplayerId;
+        addressOfPlayers[msg.sender] = currentplayerId;
 
         areyouAPlayer[msg.sender] = true;
 
@@ -148,7 +149,7 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
             currentplayerId,
             0,
             _date,
-            mintToNewPlayers,
+            gameEntryReward,
             _scores,
             0,
             false
@@ -166,8 +167,8 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
 
     function GetAplayerdetails() public view returns (Players[] memory) {
         Players[] memory ThisMember = new Players[](1);
-        uint _theId = AddressOfPlayers[msg.sender];
-        Players storage _member = IdOfPlayers[_theId];
+        uint _theId = addressOfPlayers[msg.sender];
+        Players storage _member = idOfPlayers[_theId];
         ThisMember[0] = _member;
         return ThisMember;
     }
@@ -183,12 +184,12 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
         uint AllPlayer = NumOfAllPlayers.current();
         uint addedrewards;
         for (uint i = 0; i < AllPlayer; i++) {
-            if (_id == IdOfPlayers[i + 1].PlayersId) {
-                uint currentTokens = IdOfPlayers[i + 1].TokenOwned;
+            if (_id == idOfPlayers[i + 1].PlayersId) {
+                uint currentTokens = idOfPlayers[i + 1].TokenOwned;
                 addedrewards = currentTokens + rewardtokens;
-                IdOfPlayers[i + 1].TokenOwned = addedrewards;
-                IdOfPlayers[i + 1].MyGames += 1;
-                IdOfPlayers[i + 1].Scores.push(score);
+                idOfPlayers[i + 1].TokenOwned = addedrewards;
+                idOfPlayers[i + 1].MyGames += 1;
+                idOfPlayers[i + 1].Scores.push(score);
             }
         }
         uint256 Mintmore = 5000;
@@ -213,19 +214,19 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
             "The price for the spin board is not enough"
         );
         require(areyouAPlayer[msg.sender] == true, "you are not a player");
-        Spinned[msg.sender] = true;
+        spinned[msg.sender] = true;
         uint AllPlayer = NumOfAllPlayers.current();
         for (uint i = 0; i < AllPlayer; i++) {
-            if (msg.sender == IdOfPlayers[i + 1].PlayersAddress) {
-                IdOfPlayers[i + 1].spinning = true;
+            if (msg.sender == idOfPlayers[i + 1].PlayersAddress) {
+                idOfPlayers[i + 1].spinning = true;
             }
         }
         console.log("first");
-        PeopleWhoSpinned.push(msg.sender);
+        PeopleWhospinned.push(msg.sender);
 
-        s_requestId = COORDINATOR.requestRandomWords(
+        requestId = COORDINATOR.requestRandomWords(
             keyHash,
-            s_subscriptionId,
+            subscriptionId,
             requestConfirmations,
             callbackGasLimit,
             numWords
@@ -240,31 +241,31 @@ contract GameToken is ERC20, VRFConsumerBaseV2 {
         uint256[] memory randomWords
     ) internal override {
         console.log("last");
-        s_randomLuck = (randomWords[0] % no_of_items_on_board) + 1;
-        console.log(s_randomLuck);
+        randomLuck = (randomWords[0] % boardItems) + 1;
+        console.log(randomLuck);
         ResetApplication();
     }
 
 
 
     function remove(uint index) public {
-        PeopleWhoSpinned[index] = PeopleWhoSpinned[PeopleWhoSpinned.length - 1];
-        PeopleWhoSpinned.pop();
+        PeopleWhospinned[index] = PeopleWhospinned[PeopleWhospinned.length - 1];
+        PeopleWhospinned.pop();
     }
 
 
 
 
     function ResetApplication() public {
-        for (uint i = 0; i < PeopleWhoSpinned.length; i++) {
-            address currentAddress = PeopleWhoSpinned[i];
+        for (uint i = 0; i < PeopleWhospinned.length; i++) {
+            address currentAddress = PeopleWhospinned[i];
             if (
                 areyouAPlayer[currentAddress] == true &&
-                Spinned[currentAddress] == true
+                spinned[currentAddress] == true
             ) {
-                uint _id = AddressOfPlayers[currentAddress];
-                IdOfPlayers[_id].spinning = false;
-                Spinned[currentAddress] = false;
+                uint _id = addressOfPlayers[currentAddress];
+                idOfPlayers[_id].spinning = false;
+                spinned[currentAddress] = false;
                 uint newMintingAmount = 10000 * 10**18;
                 _mint(currentAddress, newMintingAmount);
                 remove(i);
